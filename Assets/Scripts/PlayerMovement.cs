a3463dbad2ef466da2c9f3ac7f32e164e439fbc0
TrombiSE <PPPovo@mail.ru>
2020-10-07 18:15:28 +0600

    Firs not playable alpha
    0.0.0.0.01

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    public float jumpForce;
+
+    float movementx;
+
+    private void Update(){
+        movementx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump")){
+            Jump();
+        }
+    }
+
+    private void FixedUpdate(){
+        Vector2 movement = new Vector2(movementx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+    void Jump() {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+}
