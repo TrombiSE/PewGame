11cb6060469ba492e939fc493934f14404702fb8
TrombiSE <PPPovo@mail.ru>
2020-10-09 16:13:11 +0600

    Bug solving and some animations
    Adding some animation and jump bug solved

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,41 @@ public class PlayerMovement : MonoBehaviour{
     public float movementSpeed;
     public Rigidbody2D rb;
 
+    public Animator anim;
+
     public float jumpForce;
+
+    public Transform  feet;
+    public LayerMask groundLayers;
 
     float movementx;
 
     private void Update(){
         movementx = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump")){
+        if (Input.GetButtonDown("Jump") && isGrounded()){
             Jump();
         }
+
+        if (Mathf.Abs(movementx) > 0.05f)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (movementx > 0f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (movementx <0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        anim.SetBool("isGrounded", isGrounded());
     }
 
     private void FixedUpdate(){
@@ -28,4 +53,15 @@ public class PlayerMovement : MonoBehaviour{
 
         rb.velocity = movement;
     }
+
+    public bool isGrounded(){
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
