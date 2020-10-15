8cf55cefbf17e39ba9793ace5ff76a2914f48d42
TrombiSE <PPPovo@mail.ru>
2020-10-15 16:56:45 +0600

    Traps, respawn and death
    Added death and respawn system, added first visiable traps

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Destroy(gameObject);
+            LevelManager.instance.Respawn();
+        }
+    }
+
+
+}
